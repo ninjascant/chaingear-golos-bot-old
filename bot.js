a31a2e5f4c50f78057c3363499c0adc688c6191d
ninjascant <cvlt.mao@gmail.com>
2017-11-06 21:02:46 +0500

    Update: bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,112 @@
 const golos = require('golos-js')
 const Github = require('github-api-node')
+const config = require('./config.json')
 
-var github = new Github({
-  username: "goloschaingear",
-  password: "",
-  auth: "basic"
-});
+const github = new Github({
+	username: "goloschaingear",
+	password: config.git_key,
+	auth: "basic"
+})
 
-const repo = github.getRepo('goloschaingear', 'chaingear');
+const baseRepo = github.getRepo('ninjascant', 'chaingear')
+const repo = github.getRepo('goloschaingear', 'chaingear')
+const dataRepo = github.getRepo('goloschaingear', 'data')
 
 const options = {
 	author: {name: 'GolosBot', email: 'echo.from@yandex.ru'},
 	committer: {name: 'GolosBot', email: 'echo.from@yandex.ru'},
-	encode: true  
+	encode: true
 }
+
 let lastPost = {}
 
-const main = () => {
-	golos.api.getDiscussionsByCreated({"select_tags":[], "limit":10}, (err, res)=>err?console.log(err):res)
-		.then(posts=>{
-			meta = JSON.parse(posts[0].json_metadata)
-			if(posts[0].id!==lastPost.id ) {
-				const link = posts[0].permlink.replace(/[\W]/g, '')
-				const file = 'sources/' + link + '/' + link +'.toml'
+const getBranches = (repository) => {
+	return new Promise((resolve, reject) => {
+		repository.listBranches((err, branches) => err?reject(err):resolve(branches))
+	})
+}
+const createBranch = (repository, link) => {
+	return new Promise((resolve, reject) => {
+		repository.branch('from-golos', link, err => err?reject(err):resolve(link))
+	})
+}
+const writeOrUpdate = (repository, branch, link, file, text) => {
+	return new Promise((resolve, reject)=> {
+		repository.write(branch, 
+			file, 
+			text, 
+			`Commit from golosBot. Add: ${link}`, 
+			options, 
+			err => err?reject(err):resolve(`File ${link}.toml created`))
+	})
+}
+const readFile = (repository) => {
+	return new Promise((resolve, reject) => repository.read('master', 'bot-list.json', (err, res) => err?reject(err):resolve(res)))
+}
+const createPullRequest = (base, pullReq) => {
+	return new Promise((resolve, reject) => base.createPullRequest(pull, (err, res) => err?reject(err):resolve(res))
+}
 
-				repo.branch('test1', link, (err) => {
-					console.log(err?err:'Branch ' + link + ' created')
-					return repo.write(link, file, posts[0].body, 'Commit from golos', options, (err) => console.log(err?err:'File ' + link + '.toml created'));
-				})
-			} 
-			
+const main = () => {
+	golos.api.getDiscussionsByCreated({"select_tags":['ico-data'], "limit":10}, (err, res)=>err?console.log(err):res)
+		.then(posts=>{
+			const meta = JSON.parse(posts[0].json_metadata)
+			if(posts[0].id!==lastPost.id && meta.format==='markdown') {
+				const link = posts[0].title.replace(/[\W]/g, '')
+				const file = `sources/${link}/${link}.toml`
+				console.log('New post!')
+
+				getBranches(repo)
+					.then(branches => branches.indexOf(link)===-1?createBranch(repo, link):link)
+					.then(branch => writeOrUpdate(repo, link, link, file, posts[0].body))
+					.then(fileRes => {
+						console.log(fileRes)
+						const pull = {
+							title: `Add: ${link}`,
+							body: 'Pull request from chainhear-golos-bot',
+							base: 'gh-pages',
+							head: `goloschaingear:${link}`
+						}
+						return createPullRequest(baseRepo, pull)
+					})
+					.then(res => {
+						console.log(res)
+					    return golos.broadcast.comment(config.wif, 
+					    	posts[0].author, 
+								posts[0].permlink, 
+								'cyberanalytics', 
+								`re-cyberanalytics-${posts[0].permlink}-${Math.floor(Math.random() * (1000000 - 1) + 1)}`, 
+								'', 
+								'Thank you for submitting your application for listing on cyber•Fund ICO Radar.', 
+								'', 
+								(err, result) => err?console.log(err):result
+								)
+					})
+					.then(none => readFile(dataRepo))
+					.then(list => {
+						const tmp = {
+							project: link,
+							author: posts[0].author,
+							url: posts[0].url,
+							created: posts[0].created,
+							timestamp: new Date().toISOString()
+						}
+						list.push(tmp)
+						return writeOrUpdate(dataRepo, 'master', 'bot-list', 'bot-list.json', JSON.stringify(list))
+					})
+					.then(res=>{
+						console.log(res)
+						return
+					})
+					.catch(error=>console.log('Err', error))
+			}
 			lastPost.id = posts[0].id
-		    lastPost.url = posts[0].url
-		    lastPost.meta = JSON.parse(posts[0].json_metadata)
-		    return
+			lastPost.url = posts[0].url
+			lastPost.meta = JSON.parse(posts[0].json_metadata)
 		})
 }
 
-golos.api.getDiscussionsByCreated({"select_tags":[], "limit":10}, (err, res)=>err?console.log(err):res)
+golos.api.getDiscussionsByCreated({"select_tags":['ico-data'], "limit":10}, (err, res)=>err?console.log(err):res)
     .then(posts=>{
     	lastPost.id = posts[0].id
     	lastPost.url = posts[0].url
